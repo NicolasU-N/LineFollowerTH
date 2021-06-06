c5e0b3697845ea3165a635a52bee7baa54a8b9d0
Nicolás Useche <u20171154918@usco.edu.co>
2021-06-06 14:00:04 -0500

    Add Sensosrs and pwm H

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,60 @@
 #include <Arduino.h>
-/*
+
 // Pines direccion puente h
-#define MOT_DER_DIR 22 // 1 CC // 0 CW (INV)
-#define MOT_DER_PWM 4
-#define MOT_IZQ_DIR 28 // 1 CC //0 CW (INV)
-#define MOT_IZQ_PWM 5
+#define RPWM 9  // Hubungkan pin D9 dengan pin RPWM BTS7960
+#define LPWM 10 // Hubungkan pin D10 dengan pin LPWM BTS7960
+#define PWM 11  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
+
+#define S0 22  // Hubungkan pin D9 dengan pin RPWM BTS7960
+#define S1 23  // Hubungkan pin D10 dengan pin LPWM BTS7960
+#define S3 24  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
+#define S2 25  // Hubungkan pin D9 dengan pin RPWM BTS7960
+#define SIG A0 // Hubungkan pin D9 dengan pin RPWM BTS7960
 
 // Sensores
 //volatile uint16_t sensores[5];
+
 void readSensor();
 
 void setup()
 {
   Serial.begin(115200);
   // put your setup code here, to run once:
-  pinMode(MOT_DER_PWM, OUTPUT);
-  pinMode(MOT_DER_DIR, OUTPUT);
-  pinMode(MOT_IZQ_DIR, OUTPUT);
-  pinMode(MOT_IZQ_PWM, OUTPUT);
+  Serial.println("Start");
+  pinMode(RPWM, OUTPUT);
+  pinMode(PWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
+
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-
   readSensor();
-
-  digitalWrite(MOT_DER_DIR, HIGH); // Run CC
-  digitalWrite(MOT_IZQ_DIR, LOW);
-  
-  analogWrite(MOT_IZQ_PWM, 0);
-  for (int i = 0; i < 255; i++)
-  {
-    analogWrite(MOT_DER_PWM, i);
-    delay(7);
-  }
+  /*
+  motor_CW(); //Muter CW 1 detik
   delay(1000);
 
-  digitalWrite(MOT_DER_DIR, LOW); // Run CW
-  digitalWrite(MOT_IZQ_DIR, HIGH);
-
-  analogWrite(MOT_DER_PWM, 0);
-  for (int i = 0; i < 255; i++)
-  {
-    analogWrite(MOT_IZQ_PWM, i);
-    delay(7);
-  }
+  motor_stop(); //STOP 1 detik
   delay(1000);
+
+  motor_CCW(); //Muter CCW 1 detik
+  delay(1000);
+
+  motor_stop(); //STOP 1 detik
+  delay(1000);
+  */
 }
 
 void readSensor()
 {
   static unsigned long previousMillis1 = 0;
   if ((millis() - previousMillis1) > 100)
-  {    
+  {
     Serial.print(analogRead(A0));
     Serial.print(" -- ");
     Serial.print(analogRead(A1));
@@ -66,21 +67,6 @@ void readSensor()
     previousMillis1 += 100;
   }
 }
-
-*/
-
-/*
-Coding ini dibuat untuk tutorial Youtube: 
-Dibuat oleh : Yonatan K. A. Sarumaha untuk TechnoHance
-Tanggal     : 5/12/2020
-Follow IG TechnoHance     : @technohance
-Subscribe YT TechnoHance  : @technohance
-*/
-
-//inisialisasi pin yang digunakan oleh Arduino Nano
-#define RPWM 9  // Hubungkan pin D9 dengan pin RPWM BTS7960
-#define LPWM 10 // Hubungkan pin D10 dengan pin LPWM BTS7960
-#define PWM 11  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
 
 //Function buat motor muter ke CW
 void motor_CW()
@@ -113,41 +99,4 @@ void motor_stop()
   digitalWrite(RPWM, LOW);
   analogWrite(PWM, 0); //Value "0" harus tetap 0, karena motornya diperintahkan untuk STOP
   Serial.println("STOP");
-}
-
-//Inisialisasi program. Dijalankan hanya 1 kali
-void setup()
-{
-  Serial.begin(115200); //Buka serial monitor untuk melihat output ini. (CTRL+SHIFT+M)
-  Serial.println("Start");
-  pinMode(RPWM, OUTPUT);
-  pinMode(PWM, OUTPUT);
-  pinMode(LPWM, OUTPUT);
-}
-
-//Main program
-void loop()
-{
-  Serial.print((int)analogRead(A0)); // IZ
-  Serial.print(" --- ");
-  Serial.println((int)analogRead(A1)); // DER
-  motor_CW();                          //Muter CW 1 detik
-  delay(1000);
-  Serial.print((int)analogRead(A0)); // IZ
-  Serial.print(" --- ");
-  Serial.println((int)analogRead(A1)); // DER
-  motor_stop();                        //STOP 1 detik
-  delay(1000);
-  Serial.print((int)analogRead(A0)); // IZ
-  Serial.print(" --- ");
-  Serial.println((int)analogRead(A1)); // DER
-  motor_CCW();                         //Muter CCW 1 detik
-  delay(1000);
-  Serial.print((int)analogRead(A0)); // IZ
-  Serial.print(" --- ");
-  Serial.println((int)analogRead(A1)); // DER
-  motor_stop();                        //STOP 1 detik
-  delay(1000);
-
-  //Loop terus
 }
