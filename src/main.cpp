798c690f34661ba8500075e294a3493fadd9cb5f
Nicolas Useche <u20171154918@usco.edu.co>
2021-06-19 19:37:17 -0500

    Add read sensors ultrasonic

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@ LineFollowerTH
 
 TODO:
 * Agregar control PID
+* Definir encoders
+* Definir celda de carga
+* Terminar lib PWM
+* Arraque suave
 ----------------
 Pin Mapping:
 Physical Pin      Arduino Pin    Port Pin     Function
@@ -44,35 +48,36 @@ Physical Pin      Arduino Pin    Port Pin     Function
 48                D48            PL1          BTNBACKB //Atras
 49                D49            PL0          BTNFRONTF //Adelante
 50                D50            PB3          BTNFRONTB //Atras
+
 */
 
 #include <Arduino.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <ADC.h>
 
-//Pines Mux
-#define S0D PA0  //22  // OUTPUT
-#define S1D PA1  //23  // OUTPUT
-#define S3D PA2  //24  // OUTPUT
-#define S2D PA3  //25  // OUTPUT
-#define SIGD PF0 //A0 // INPUT
+#define S0D PORTA0 //22  // OUTPUT
+#define S1D PORTA1 //23  // OUTPUT
+#define S3D PORTA2 //24  // OUTPUT
+#define S2D PORTA3 //25  // OUTPUT
+#define SIGD 0     //A0 // INPUT
 
-#define S0I PA4  //22  // OUTPUT
-#define S1I PA5  //23  // OUTPUT
-#define S3I PA6  //24  // OUTPUT
-#define S2I PA7  //25  // OUTPUT
-#define SIGI PF1 //A0 // INPUT
+#define S0I PORTA4 //22  // OUTPUT
+#define S1I PORTA5 //23  // OUTPUT
+#define S3I PORTA6 //24  // OUTPUT
+#define S2I PORTA7 //25  // OUTPUT
+#define SIGI 1     //A0 // INPUT
 
 //Pines Sensor distancia infrarojo
-#define IRDISF PF2 //Input Analog
-#define IRDISB PF3 //Input Analog
+#define IRDISF 2 //PF2 //Input Analog
+#define IRDISB 3 //PF3 //Input Analog
 
 // Pines direccion puente h
-#define RPWMIZQ PL7 //42 // Hubungkan pin D9 dengan pin RPWM BTS7960
-#define LPWMIZQ PL6 //43 // Hubungkan pin D10 dengan pin LPWM BTS7960
-#define PWMIZQ0 PE4 //2  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
-#define PWMIZQ1 PE5 //3  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
+#define RPWMIZQ PORTL7 //42 // Hubungkan pin D9 dengan pin RPWM BTS7960
+#define LPWMIZQ PORTL6 //43 // Hubungkan pin D10 dengan pin LPWM BTS7960
+#define PWMIZQ0 PORTE4 //2  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
+#define PWMIZQ1 PORTE5 //3  // Hubungkan pin D11 dengan pin R_EN and L_EN BTS7960. Harus menggunakan pin PWM dari MicroController
 
 #define RPWMDER PL5 // Hubungkan pin D9 dengan pin RPWM BTS7960
 #define LPWMDER PL4 // Hubungkan pin D10 dengan pin LPWM BTS7960
@@ -86,27 +91,55 @@ Physical Pin      Arduino Pin    Port Pin     Function
 #define BTNFRONTF PL0 // Input
 #define BTNFRONTB PB3 // Input
 
+#define TRIG1BACKIZQ PORTC7
+#define ECHO1BACKIZQ 31 // Ultrasonic sensor 1
+
+#define TRIG2LATIZQ PORTC5
+#define ECHO2LATIZQECHO 33 // Ultrasonic sensor 1
+
+#define TRIG3FRONTLATIZQ PORTC3
+#define ECHO3FRONTLATIZQ 35 // Ultrasonic sensor 1
+
+#define TRIG4FRONTLATDER PORTC1
+#define ECHO4FRONTLATDER 37 // Ultrasonic sensor 1
+
+#define TRIG5LATDER PORTD7
+#define ECHO5LATDER 39 // Ultrasonic sensor 1
+
+#define TRIG6BACKDER PORTG1
+#define ECHO6BACKDER 41 // Ultrasonic sensor 1
+
 #define CW 0
 #define CCW 1
 #define STOP 2
-
-#define TRIG 30
-#define ECHO 31 // Ultrasonic sensor 1
 
 uint8_t state = 1;
 uint8_t fadeValue;
 uint8_t fadeValue1;
 uint8_t fadeValue2;
 
-// Sensores
-//volatile uint16_t sensores[5];
+// Sensores izquierdos
+volatile uint16_t lineSenBackIzq[5];
+volatile uint16_t lineSenLatIzq[5];
+volatile uint16_t lineSenFrontIzq[5];
 
-void readLineSensor();
+volatile uint16_t lineSenBackDer[5];
+volatile uint16_t lineSenLatDer[5];
+volatile uint16_t linesenFrontDer[5];
+
+volatile float distUltra[6];
+
+volatile uint16_t disIrSenValue[2];
+
+//void readLineSensor();
 void motor_stop();
 void motor_CW();
 void motor_CCW();
 void changeState();
-void readUltrasonicSensor();
+void readUltraSen();
+void readDisIrSen();
+void readMuxIzq();
+void readMuxDer();
 
 void setup()
 {
@@ -115,16 +148,28 @@ void setup()
   // put your setup code here, to run once:
   Serial.println("Start");
 
+  //CONFIGURAR ADC (SENSORES IR)
+  ADCInit();
+
   //PORTA = 0x00; // OFF ALL
 
   //CONFIGURACION PINES DE SALIDA
-  DDRA = 0xFF;                                                                 // OUTPUT ALL pines del puerto A
+  DDRA = 0xFF; // OUTPUT ALL pines del puerto A
+  DDRC |= (1 << DDC7) | (1 << DDC3) | (1 << DDC5) | (1 << DDC1);
+  DDRD |= (1 << DDD7);
   DDRL |= (1 << DDL1) | (1 << DDL2) | (1 << DDL3) | (1 << DDL4) | (1 << DDL5); //Pin 3 del puerto D ,//Pin 4 del puerto D, Pin 5 del puerto D, Pin 6 del puerto D
   DDRE |= (1 << DDE3) | (1 << DDE4) | (1 << DDE5);
-  DDRG |= (1 << DDG5);
+  DDRG |= (1 << DDG5) | (1 << DDG1);
 
   //CONFIGURACION PINES DE ENTRADA
   DDRL &= ~((1 << DDL1) | (1 << DDL2) | (1 << DDL0) | (1 << DDL0));
+
+  //PINES PULL UP ULTRASONICOS
+  DDRC &= ~((1 << DDC6) | (1 << DDC4) | (1 << DDC2) | (1 << DDC0));
+  DDRG &= ~((1 << DDG2) | (1 << DDG0));
+
+  PORTC |= (1 << PORTC6) | (1 << PORTC4) | (1 << PORTC2) | (1 << PORTC0);
+  PORTG |= (1 << PORTG2) | (1 << PORTG0);
 
   /*
   pinMode(RPWMIZQ, OUTPUT);
@@ -148,8 +193,10 @@ void setup()
   pinMode(S3D, OUTPUT);
   */
 
-  pinMode(TRIG, OUTPUT); // ultrasonic sensor
-  pinMode(ECHO, INPUT_PULLUP);
+  // ultrasonic sensor
+  //pinMode(TRIG, OUTPUT);
+  //pinMode(ECHO, INPUT_PULLUP);
+
   sei();
 }
 
@@ -158,7 +205,13 @@ void loop()
   // put your main code here, to run repeatedly:
   //readLineSensor();
   changeState(); // Change state
-  //readUltrasonicSensor();
+
+  readUltraSen();
+  readDisIrSen();
+
+  readMuxIzq();
+  readMuxDer();
+
   switch (state)
   {
   case CW:
@@ -173,45 +226,134 @@ void loop()
     motor_CCW();
     break;
   }
-  if (state > 2)
-  {
-    state = 0;
-  }
 }
 
+/*
 void readLineSensor()
 {
   static unsigned long previousMillis1 = 0;
   if ((millis() - previousMillis1) > 100)
   {
-    Serial.print(analogRead(A0));
+
+    Serial.print(ADCGetData(0));
     Serial.print(" -- ");
-    Serial.print(analogRead(A1));
+    Serial.print(ADCGetData(1));
     Serial.print(" -- ");
-    Serial.print(analogRead(A2));
+    Serial.print(ADCGetData(2));
     Serial.print(" -- ");
-    Serial.print(analogRead(A3));
+    Serial.print(ADCGetData(3));
     Serial.print(" -- ");
-    Serial.println(analogRead(A4));
+    Serial.println(ADCGetData(5));
     previousMillis1 += 100;
   }
 }
+*/
 
-void readUltrasonicSensor()
+void readUltraSen()
 {
   static unsigned long previousMillis1 = 0;
   if ((millis() - previousMillis1) > 1000)
   {
-    digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
-    delayMicroseconds(2);
+    //-------------------------------------------------------------------------------
+    PORTC &= ~(1 << TRIG1BACKIZQ);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
 
-    digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
-    delayMicroseconds(20);
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTC |= (1 << TRIG1BACKIZQ);
+    _delay_us(20);
 
-    digitalWrite(TRIG, LOW);                     // Send pin low again
-    float distance = pulseIn(ECHO, HIGH, 26000); // Read in times pulse
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTC &= ~(1 << TRIG1BACKIZQ);
+    distUltra[0] = pulseIn(ECHO1BACKIZQ, HIGH, 26000); // Read in times pulse
 
-    distance = distance / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+    distUltra[0] = distUltra[0] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    PORTC &= ~(1 << TRIG2LATIZQ);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
+
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTC |= (1 << TRIG2LATIZQ);
+    _delay_us(20);
+
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTC &= ~(1 << TRIG2LATIZQ);
+    distUltra[1] = pulseIn(ECHO2LATIZQECHO, HIGH, 26000); // Read in times pulse
+
+    distUltra[1] = distUltra[1] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    PORTC &= ~(1 << TRIG3FRONTLATIZQ);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
+
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTC |= (1 << TRIG3FRONTLATIZQ);
+    _delay_us(20);
+
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTC &= ~(1 << TRIG3FRONTLATIZQ);
+    distUltra[2] = pulseIn(ECHO3FRONTLATIZQ, HIGH, 26000); // Read in times pulse
+
+    distUltra[2] = distUltra[2] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    PORTC &= ~(1 << TRIG4FRONTLATDER);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
+
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTC |= (1 << TRIG4FRONTLATDER);
+    _delay_us(20);
+
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTC &= ~(1 << TRIG4FRONTLATDER);
+    distUltra[3] = pulseIn(ECHO4FRONTLATDER, HIGH, 26300); // Read in times pulse
+
+    distUltra[3] = distUltra[3] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    PORTD &= ~(1 << TRIG5LATDER);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
+
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTD |= (1 << TRIG5LATDER);
+    _delay_us(20);
+
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTD &= ~(1 << TRIG5LATDER);
+    distUltra[4] = pulseIn(ECHO5LATDER, HIGH, 26000); // Read in times pulse
+
+    distUltra[4] = distUltra[4] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+
+    //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
+    PORTG &= ~(1 << TRIG6BACKDER);
+    //digitalWrite(TRIG, LOW); // Set the trigger pin to low for 2uS
+    _delay_us(2);
+
+    //digitalWrite(TRIG, HIGH); // Send a 10uS high to trigger ranging
+    PORTG |= (1 << TRIG6BACKDER);
+    _delay_us(20);
+
+    //digitalWrite(TRIG, LOW);                     // Send pin low again
+    PORTG &= ~(1 << TRIG6BACKDER);
+    distUltra[5] = pulseIn(ECHO6BACKDER, HIGH, 26000); // Read in times pulse
+
+    distUltra[5] = distUltra[5] / 58; //13.3511 instead of 58 because the speed of a soundwave in water is far bigger than in air
+
+    //-------------------------------------------------------------------------------
+
+    // PRINT ARRAY
+    Serial.print("-- DISTANCE ULTRASONIC SENSOR: ");
+    for (size_t i = 0; i < 6; i++)
+    {
+      Serial.println(distUltra[i]);
+    }
     previousMillis1 += 1000;
   }
 }
@@ -221,7 +363,15 @@ void changeState()
   static unsigned long previousMillis3 = 0;
   if ((millis() - previousMillis3) > 3000)
   {
-    state = state + 1;
+    if (state > 2)
+    {
+      state = 0;
+    }
+    else
+    {
+      state = state + 1;
+    }
+
     //fadeValue = 0;
     //fadeValue1 = 0;
 
@@ -394,14 +544,71 @@ void motor_stop()
   @brief Leer mux 1  
   @param canal
 */
-void readMux()
+void readMuxIzq()
 {
   for (size_t i = 0; i < 16; i++)
   {
-    digitalWrite(S0, i & 0x01);
+    PORTA = (i & 0x01 << S0I) | (i & 0x02 << S1I) | (i & 0x04 << S2I) | (i & 0x08 << S3I);
+    /*digitalWrite(S0, i & 0x01);
     digitalWrite(S1, i & 0x02);
     digitalWrite(S2, i & 0x04);
-    digitalWrite(S3, i & 0x08);
+    digitalWrite(S3, i & 0x08);*/
+    if (i < 5)
+    {
+      lineSenBackIzq[i] = analogRead(SIGI); // leer canal
+      Serial.print(lineSenBackIzq[i]);
+      Serial.print("---");
+    }
+    else if (i > 5 && i < 10)
+    {
+      lineSenLatIzq[i - 5] = analogRead(SIGI); // leer canal
+      Serial.print(lineSenLatIzq[i - 5]);
+      Serial.print("---");
+    }
+    else if (i > 10 && i < 15)
+    {
+      lineSenFrontIzq[i - 10] = analogRead(SIGI);
+      Serial.print(lineSenFrontIzq[i - 10]);
+      Serial.print("---");
+    }
   }
-  analogRead(SIG); // leer canal
+  Serial.println("");
+}
+
+/*
+  @brief Leer mux 2  
+  @param canal
+*/
+void readMuxDer()
+{
+  for (size_t i = 0; i < 16; i++)
+  {
+    PORTA = (i & 0x01 << S0D) | (i & 0x02 << S1D) | (i & 0x04 << S2D) | (i & 0x08 << S3D);
+    /*digitalWrite(S0, i & 0x01);
+    digitalWrite(S1, i & 0x02);
+    digitalWrite(S2, i & 0x04);
+    digitalWrite(S3, i & 0x08);*/
+    if (i < 5)
+    {
+      lineSenBackDer[i] = ADCGetData(SIGD); // leer canal
+    }
+    else if (i > 5 && i < 10)
+    {
+      lineSenLatDer[i - 5] = ADCGetData(SIGD); // leer canal
+    }
+    else if (i > 10 && i < 15)
+    {
+      linesenFrontDer[i - 10] = ADCGetData(SIGD);
+    }
+  }
+}
+
+/*
+  @brief Leer sensores distancia infrarojo  
+  @param canal
+*/
+void readDisIrSen()
+{
+  disIrSenValue[0] = ADCGetData(IRDISF); // leer dis front
+  disIrSenValue[1] = ADCGetData(IRDISB); // leer dis back
 }
